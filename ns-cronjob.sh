4baa42b0ca98c35205c18d0f3e561da3e8168d5f
Ryan Butler <ryan.butler@thinkahead.com>
2016-08-31 10:22:13 -0500

    Update ns-cronjob.sh

--- ns-cronjob.sh
+++ ns-cronjob.sh
@@ -5,4 +5,5 @@
 
 #Normal usage
 /root/ns-letsencrypt/letsencrypt.sh/letsencrypt.sh -c -f /root/ns-letsencrypt/config.sh -k /root/ns-letsencrypt/ns-hook.sh
+#Cleanup unused certs
 /root/ns-letsencrypt/letsencrypt.sh/letsencrypt.sh -gc -f /root/ns-letsencrypt/config.sh
